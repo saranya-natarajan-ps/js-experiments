f6fc3b41dee05d2a522fb161464b416242d615af
Saranya Natarajan <saranya-natarajan@pluralsight.com>
2024-05-08 09:45:03 +0530

    Added Celsius temperature into Farenheit script

---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,7 @@
+//convert a Celsius temperature into Farenheit
+
+const cel = 35;
+let far;
+far=((cel * 9.0 / 5.0) + 32.0);
+
+console.log("35 degree celsius is equivallent to " +far+ " degree farenheit");
