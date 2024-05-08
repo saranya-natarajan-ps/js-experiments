c7a6e1af4302a17f309a8bb8c19f34eb96910feb
Saranya Natarajan <saranya-natarajan@pluralsight.com>
2024-05-08 09:27:34 +0530

    Added tips and area of circle experiments

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,7 @@
+const radius = 10;
+const pi = Math.PI;
+const areaOfCircle = (radius, pi) => {
+   return pi * radius * radius;
+};
+console.log("The area of a circle with radius 10 is: " + areaOfCircle(radius, pi));
+
---
+++ MathScripts/tips.js
@@ -0,0 +1,23 @@
+//sample inputs
+
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.00;
+
+//calculations 
+
+let totalDue = foodCost + tax + tip;
+
+//output
+
+//console.log("The total due is " +totalDue);
+
+
+console.log("Food cost is " + foodCost + " and tax is " + tax);
+ //console.log("Tip is " + tip);
+ 
+ console.log("Tip is " + tip.toFixed(0));
+
+
+ console.log("Total Due is " + totalDue);
+ 
